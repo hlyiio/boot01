947d73c7485dd5a6d4202ab505e0d3cbc870edbb
hlyiio <179664484@qq.com>
2022-07-13 18:26:29 +0800

    first commit

---
+++ src/main/java/com/boot/controller/hello.java
@@ -0,0 +1,12 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @RequestMapping("/hello")
+    public String handle01(){
+    return "hello,springboot02!";
+    }
+}
